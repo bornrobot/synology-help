b4cf463f33595586744f06b6f5bacb07dcb0ea7a
bornrobot <ed@edr.so>
2019-07-17 11:47:50 +0100

    Update flush-dns-cache.sh

--- flush-dns-cache.sh
+++ flush-dns-cache.sh
@@ -1,3 +1,7 @@
-#Flush the DNS Server cache over SSH...
+#Flush the DNS Server cache
+#
+#1. Emable SSH via the web interface.
+#2. Connnect to the server via SSH
+#3. Run this command...
 
 sudo /volume1/@appstore/DNSServer/script/flushcache.sh
