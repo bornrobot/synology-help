9e505841e2ba32d846b73b097c0fa672e046a9d1
bornrobot <ed@edr.so>
2019-07-17 11:51:19 +0100

    Update flush-dns-cache.sh

--- flush-dns-cache.sh
+++ flush-dns-cache.sh
@@ -1,9 +1,9 @@
-#Flush the DNS Server cache
+# Flush the DNS Server cache on the Synology Disk Station 
 #
-#1. Emable the SSH service via the web interface (Control Panel -> Terminal & SNMP).
-#2. Connnect to the server via SSH
-#3. Run this command...
+# 1. Emable the SSH service via the web interface (Control Panel -> Terminal & SNMP).
+# 2. Connnect to the server via SSH
+# 3. Run this command...
 
 sudo /volume1/@appstore/DNSServer/script/flushcache.sh
 
-#4. Disable the SSH service again via the web interface (Control Panel -> Terminal & SNMP).
+# 4. Disable the SSH service again via the web interface (Control Panel -> Terminal & SNMP).
